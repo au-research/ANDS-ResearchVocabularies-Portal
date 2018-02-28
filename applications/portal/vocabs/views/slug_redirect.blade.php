9118eb587ce523d237f2e07fe2141c8f8565ac2e
Richard Walker <Richard.Walker@ands.org.au>
2018-02-28 11:48:38 +1100

    Add placeholder slug_redirect.blade.php in case needed later
    Add a slug_redirect.blade.php which presents a redirection page.
    See the comment in vocabs.php/view() that shows how it would
    work.
    
    Not used for now; revisit later as needed.

---
+++ applications/portal/vocabs/views/slug_redirect.blade.php
@@ -0,0 +1,31 @@
+@extends('layout/vocab_layout')
+@section('content')
+<article>
+
+    <section class="section swatch-white element-short-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <header class="text-center element-normal-top element-no-bottom not-condensed os-animation animated fadeInUp" data-os-animation="fadeInUp" data-os-animation-delay="0s" style="-webkit-animation: 0s;">
+                        <h1 class="bigger hairline bordered bordered-normal os-animation animated fadeIn" data-os-animation="fadeIn" data-os-animation-delay="0s" style="-webkit-animation: 0s;"> Redirection notice </h1>
+                    </header>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <p>The URLs of vocabulary view pages on {{$title}} have changed.</p>
+                    <p>You can access the view page for this vocabulary using either of these URLs:</p>
+                    <ul>
+                        <li>ID-based: <a href="{{base_url().'viewById/'.$id}}">{{base_url().'viewById/'.$id}}</a></li>
+                        <li>Slug-based: <a href="{{base_url().'viewBySlug/'.$slug}}">{{base_url().'viewBySlug/'.$slug}}</a></li>
+                    </ul>
+                    <p>Please update your bookmarks.</p>
+                    <p>You will be redirected in ten seconds.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</article>
+@stop
+
