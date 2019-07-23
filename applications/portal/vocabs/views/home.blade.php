66d118e909e03744ed06fb6113c3b6575efcc214
Richard Walker <Richard.Walker@ands.org.au>
2019-07-23 12:00:22 +1000

    On the home page only, autofocus to the search query text input
    Just on the home page, apply autofocus to the text input for the
    search query.
    
    In future: consider whether this should be applied to other pages.
    
    Reformat and tidy the home blade some more.

--- applications/portal/vocabs/views/home.blade.php
+++ applications/portal/vocabs/views/home.blade.php
@@ -4,8 +4,7 @@
   <section class="section swatch-dark-blue search-section
                   section-text-shadow section-inner-shadow">
 
-    <div id="banner-image" class="background-media" style="">
-    </div>
+    <div id="banner-image" class="background-media"></div>
 
     <div class="background-overlay grid-overlay-30"
          style="background-color: rgba(0,0,0,0.4);"></div>
@@ -13,22 +12,22 @@
       <div class="row">
 
         <div class="col-md-6">
-          <form action=""
-                ng-submit="$event.preventDefault();search()"
+          <form ng-submit="$event.preventDefault();search()"
                 style="padding-top:55px">
             <div class="input-group">
               <input type="text" class="form-control"
+                     autofocus="autofocus"
                      placeholder="Search for a vocabulary or a concept"
                      ng-model="form.query">
               <span class="input-group-btn">
                 <button class="btn btn-primary" type="button"
-                        ng-click="search()"><i class="fa fa-search"></i> Search</button>
+                        ng-click="search()"
+                ><i class="fa fa-search"></i> Search</button>
               </span>
             </div>
           </form>
           <div class="pull-right">
-            <a href="{{ portal_url('search') }}">Browse all
-              vocabularies</a>
+            <a href="{{ portal_url('search') }}">Browse all vocabularies</a>
           </div>
         </div>
 
@@ -36,9 +35,8 @@
           <header class="animated fadeIn">
             <h1 class="big">Research Vocabularies Australia</h1>
             <p class="big hairline">
-              helps you find, access, and reuse vocabularies
-              for research.
-            </p>
+              helps you find, access, and reuse vocabularies for
+              research.</p>
           </header>
         </div>
 
@@ -53,13 +51,11 @@
           <header class="text-center element-short-top
                          element-no-bottom os-animation
                          condensed animated fadeIn"
-                  data-os-animation="fadeIn"
-                  data-os-animation-delay="0s"
+                  data-os-animation="fadeIn" data-os-animation-delay="0s"
                   style="-webkit-animation: 0s;">
-            <h1 class="big"> Get Involved </h1>
+            <h1 class="big">Get Involved</h1>
           </header>
-          <div class="row" data-os-animation=""
-               data-os-animation-delay="">
+          <div class="row">
             <div class="col-md-3">
               <a href="{{ portal_url('vocabs/page/contribute') }}">
                 <div class="element-medium-top
@@ -71,16 +67,13 @@
                   <div class="box-square box-medium box-simple">
                     <div class="box-dummy"></div>
                     <div class="box-inner grid-overlay-0">
-                      <i class="fa fa-cloud-upload
-                                icon-portal"></i>
+                      <i class="fa fa-cloud-upload icon-portal"></i>
                     </div>
                   </div>
-                  <h3 class="normal bold bordered
-                             bordered-small">Publish a
+                  <h3 class="normal bold bordered bordered-small">Publish a
                     vocabulary</h3>
-                  <p class="">Upload, describe and publish
-                    your vocabularies to Research Vocabularies
-                    Australia</p>
+                  <p>Upload, describe and publish your
+                    vocabularies to Research Vocabularies Australia</p>
                 </div>
               </a>
             </div>
@@ -95,15 +88,13 @@
                   <div class="box-square box-medium box-simple">
                     <div class="box-dummy"></div>
                     <div class="box-inner grid-overlay-0">
-                      <i class="fa fa-language
-                                icon-portal"></i>
+                      <i class="fa fa-language icon-portal"></i>
                     </div>
                   </div>
                   <h3 class="normal bold bordered
                              bordered-small">Use a vocabulary</h3>
-                  <p class="">Understand how you can utilise
-                    Research Vocabulary Australia
-                    vocabularies</p>
+                  <p>Understand how you can utilise Research
+                    Vocabulary Australia vocabularies</p>
                 </div>
               </a>
             </div>
@@ -124,9 +115,9 @@
                   <h3 class="normal bold bordered
                              bordered-small">Explore widgetable
                     vocabularies</h3>
-                  <p class="">Discover vocabularies that can
-                    be readily used in your system using
-                    our vocabulary widget</p>
+                  <p>Discover vocabularies that can be
+                    readily used in your system using our vocabulary
+                    widget</p>
                 </div>
               </a>
             </div>
@@ -147,9 +138,8 @@
                   </div>
                   <h3 class="normal bold bordered
                              bordered-small">Provide feedback</h3>
-                  <p class="">Help Research Vocabularies
-                    Australia to grow into a comprehensive
-                    vocabulary portal</p>
+                  <p>Help Research Vocabularies Australia to
+                    grow into a comprehensive vocabulary portal</p>
                 </div>
               </a>
             </div>
