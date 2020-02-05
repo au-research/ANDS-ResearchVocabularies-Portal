9184044885c8203298b84da9acdd3ab6507650ad
Richard Walker <Richard.Walker@ands.org.au>
2020-02-05 16:04:37 +1100

    RVA-25 Fetch metadata from PP: add all languages, not just primary
    When pre-filling the CMS with data from PoolParty, include all of the
    project's languages, not just the primary language.  (Make sure that
    the primary language is the first one in the CMS's list, as that's
    what the Registry expects.)
    
    CC-2663

--- applications/portal/vocabs/assets/js/vocabs_cms.js
+++ applications/portal/vocabs/assets/js/vocabs_cms.js
@@ -755,6 +755,27 @@
                 return;
             }
 
+            // Get the primary language, so we can use it to fetch
+            // other values. Defaults to 'en'.
+            // Get the other languages while we're here.
+            var primaryLanguage = 'en';
+            if (data['dcterms:language']) {
+                var primaryLanguage = $scope.choose(
+                    data['dcterms:language'])[0];
+                $scope.vocab.language = [];
+                $scope.vocab.language.push(primaryLanguage);
+                if (data['ppcl:availablelanguages']) {
+                    var otherLanguages = $scope.choose(
+                        data['ppcl:availablelanguages']); {
+                            angular.forEach(otherLanguages, function (lang) {
+                                if (lang != primaryLanguage) {
+                                    $scope.vocab.language.push(lang);
+                                }
+                            });
+                        }
+                }
+            }
+
             if (data['dcterms:title']) {
                 $scope.vocab.title = $scope.choose(data['dcterms:title']);
                 if (angular.isArray($scope.vocab.title)) $scope.vocab.title = $scope.vocab.title[0];
@@ -777,14 +798,6 @@
                             subject_iri: '',
                             subject_notation: ''
                         });
-                });
-            }
-
-            if (data['dcterms:language']) {
-                var chosen = $scope.choose(data['dcterms:language']);
-                $scope.vocab.language = [];
-                angular.forEach(chosen, function (lang) {
-                    $scope.vocab.language.push(lang);
                 });
             }
 
