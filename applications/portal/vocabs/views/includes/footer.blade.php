489d88cba0ca586270b08f4c54c48845473536de
Richard Walker <Richard.Walker@ands.org.au>
2020-06-17 14:43:13 +1000

    CC-2708 Redo change to footer link "Developers"
    Sigh, my manual merging of develop into the rebranding branch caused a
    regression, i.e., the "Developers" link got put back to the old URL.
    
    Once again, change the footer link "Developers" to point to
    documentation.ardc.edu.au.
    
    RVA-81

--- applications/portal/vocabs/views/includes/footer.blade.php
+++ applications/portal/vocabs/views/includes/footer.blade.php
@@ -55,7 +55,7 @@
             <h5 class="sidebar-header">External Resources</h5>
             <ul>
               <li class="cat-item"> <a href="https://ardc.edu.au/" title="" target="_blank">ARDC Website</a> </li>
-              <li class="cat-item"> <a href="http://developers.ands.org.au" title="" target="_blank">Developers</a> </li>
+              <li class="cat-item"> <a href="https://documentation.ardc.edu.au/" title="" target="_blank">Developers</a> </li>
               <!-- <li class="cat-item"> <a href="{{base_url('registry/')}}" title="">ARDC Online Services</a> </li> -->
               @if(isset($ro) && $ro->core['id'])
                 <li class="cat-item"> <a href="{{base_url('registry/registry_object/view/')}}/<?=$this->ro->id?>" title="">Registry View</a> </li>
