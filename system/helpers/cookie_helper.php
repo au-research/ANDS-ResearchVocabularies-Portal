53c43b2a48852351b9fb01182881f356f1c6adf4
Richard Walker <Richard.Walker@ands.org.au>
2022-09-14 14:51:04 +1000

    CC-2901 Add SameSite cookie attribute
    Add the SameSite attribute to the cookies we generate.
    
    Because implementation depends on the version of PHP, incorporate the
    workaround from upstream CodeIgniter.
    (See CodeIgniter commit 9b8f2b7a8405acd1b8ad5956ada3d84472b1e8ae.)
    
    But we incorporate also our own fixes for apparent defects in that
    upstream code; see:
    
    https://github.com/bcit-ci/CodeIgniter/pull/6025
    https://github.com/bcit-ci/CodeIgniter/issues/6159
    
    Note that, following upstream, we also add the HttpOnly attribute to
    the session (authentication) cookie.
    
    RVA-244

--- system/helpers/cookie_helper.php
+++ system/helpers/cookie_helper.php
@@ -27,28 +27,31 @@
 
 // ------------------------------------------------------------------------
 
-/**
- * Set cookie
- *
- * Accepts six parameter, or you can submit an associative
- * array in the first parameter containing all the values.
- *
- * @access	public
- * @param	mixed
- * @param	string	the value of the cookie
- * @param	string	the number of seconds until expiration
- * @param	string	the cookie domain.  Usually:  .yourdomain.com
- * @param	string	the cookie path
- * @param	string	the cookie prefix
- * @return	void
- */
+// ARDC CC-2901 Incorporate upstream changes to add extra parameters
+// $httponly and $samesite, because we need $samesite!
+
 if ( ! function_exists('set_cookie'))
 {
-	function set_cookie($name = '', $value = '', $expire = '', $domain = '', $path = '/', $prefix = '', $secure = FALSE)
+	/**
+	 * Set cookie
+	 *
+	 * Accepts seven parameters, or you can submit an associative
+	 * array in the first parameter containing all the values.
+	 *
+	 * @param	mixed
+	 * @param	string	the value of the cookie
+	 * @param	int	the number of seconds until expiration
+	 * @param	string	the cookie domain.  Usually:  .yourdomain.com
+	 * @param	string	the cookie path
+	 * @param	string	the cookie prefix
+	 * @param	bool	true makes the cookie secure
+	 * @param	bool	true makes the cookie accessible via http(s) only (no javascript)
+	 * @return	void
+	 */
+	function set_cookie($name, $value = '', $expire = 0, $domain = '', $path = '/', $prefix = '', $secure = NULL, $httponly = NULL, $samesite = NULL)
 	{
 		// Set the config file options
-		$CI =& get_instance();
-		$CI->input->set_cookie($name, $value, $expire, $domain, $path, $prefix, $secure);
+		get_instance()->input->set_cookie($name, $value, $expire, $domain, $path, $prefix, $secure, $httponly, $samesite);
 	}
 }
 
